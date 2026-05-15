6e427e6e6f5a8845de93882c0896c30328520dc5
Aayush Yadav <aayushahir4169yadav@gmail.com>
2026-05-15 10:48:59 +0530

    Add nextLargerNodes method to find greater values
    Implemented a method to find the next greater node values in a linked list by reversing the list and using a stack.

--- LinkedList/findnextgreaternode.js
+++ LinkedList/findnextgreaternode.js
@@ -1,1 +1,55 @@
 
+class Solution {
+    public ListNode reverse (ListNode head ){
+        ListNode current= head;
+       ListNode prev = null ;
+        ListNode next = null;
+        while(current!=null){
+            next = current.next;
+            current.next = prev ;
+            prev = current ;
+            current = next ;
+
+        }
+        return prev;
+
+    }
+    public int[] nextLargerNodes(ListNode head) {
+        ListNode head2 = reverse(head);
+        Stack<ListNode> st = new Stack<>();
+       
+        ListNode temp = head2;
+        int size =0;
+        while(temp!=null){
+            size++;
+            temp= temp.next;
+            
+        }
+        int arr[] = new int [size];
+        ListNode temp2 = head2;
+        while(temp2!=null){
+           
+            
+                while(st.size()>0 &&st.peek().val<=temp2.val){
+                    st.pop();
+                }
+                if(st.size()==0){
+                    arr[size-1]=0;
+                }
+                else{
+                    arr[size-1]=st.peek().val;
+
+                }
+                
+            st.push(temp2);
+            temp2= temp2.next;;
+            size--;
+
+        }
+        return arr;
+        
+        
+
+        
+    }
+}
