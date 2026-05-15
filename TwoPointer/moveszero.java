77915528ce6c08fee9bac5b30e3446db66025e90
Aayush Yadav <aayushahir4169yadav@gmail.com>
2026-05-15 10:43:26 +0530

    Update moveszero.java

--- TwoPointer/moveszero.java
+++ TwoPointer/moveszero.java
@@ -1,1 +1,24 @@
 
+class Solution {
+    public void moveZeroes(int[] arr) {
+       int  i=0;  
+       int  j =1; 
+       while(j<arr.length){
+        if(arr[i]== 0 && arr[j]!=0){
+            int temp = arr[i];
+            arr[i] =arr[j];
+            arr[j] = temp;
+        }
+       else if(arr[i]==0 && arr[j]==0){
+            j++;
+        }
+        
+           else{
+            i++;
+            j++;
+
+           }
+        
+       }
+    }
+}
